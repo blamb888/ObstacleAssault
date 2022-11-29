e324bd0eaf28c55251b48a5e7ca66f1c2d505d19
Brandon Lamb <blamb888@gmail.com>
2022-11-29 18:20:24 +0900

    Add DistanceMoved and MoveDistance to MovingPlatform

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -15,6 +15,8 @@ AMovingPlatform::AMovingPlatform()
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
+
+	StartLocation = GetActorLocation();
 }
 
 // Called every frame
@@ -22,21 +24,21 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	FVector LocalVector = MyVector;
-
-	LocalVector.Z = LocalVector.Z + 100;
-
-	MyVector.Y = MyVector.Y + 1;
-
-	SetActorLocation(LocalVector);	
-
 	// Move platform forwards
 		// Get current location
+	FVector CurrentLocation = GetActorLocation();
 		// Add vector to that location
+	CurrentLocation = CurrentLocation + (PlatformVelocity * DeltaTime);
 		// Set the location
+	SetActorLocation(CurrentLocation);
 	// Send Platform back if gone too far
 		// Check how far we've moved
+	float DistanceMoved = FVector::Dist(CurrentLocation, StartLocation);
+	if (DistanceMoved > MoveDistance)  {
 		// Reverse direction of motion if gone too far
+		PlatformVelocity = -PlatformVelocity;
+		StartLocation = CurrentLocation;
+	}
 
 }
 
